0774a6a5c10220cc607a2529728fc97ef3e41c76
Brightbite <zindybiteme@gmail.com>
2017-09-11 18:18:40 +1000

    Assets update

--- assets/src/juser.js
+++ assets/src/juser.js
@@ -1,42 +1,139 @@
-<<<<<<< HEAD
 $(document).ready(function(){
-  //console.log('document load done.');
+  console.log('document load done.');
   userlist();
+
 
   $('#btn-register').click(function(){
       register();
   });
+
+  $('#btn-register2').click(function(){
+      register2();
+  });
+
+  $('#btn-signIn').click(function(){
+      SignIn();
+  });
+
+  $('#clk-Product').click(function(){
+      showProduct();
+  });
+
 });
-//show ADD new user popup modal
-function Addnewuser(){
-     $('#myModalLabel').text('ADD NEW USER');
-}
+// show ADD new user popup modal
+  function Addnewuser(){
+       $('#myModalLabel').text('ADD NEW USER');
+  }
+
+  function showProduct(){
+      var prod_ID = $('#prod_ID').val();
+      console.log('Product ID# '+ prod_ID);
+      $.ajax({
+        url:'product',                                       //route
+        data:{prod_ID:prod_ID},
+        method:"GET"
+      })
+      .done(function(pResult) {
+          console.log('done');
+          // window.location='product';
+          //  $('#prod_page').html(pResult);
+
+      })
+      .fail(function(jqXHR, textStatus){
+
+          alert('Error:'+ jqXHR +' '+ textStatus);
+
+      });
+  }
+//
+// function Cart(){
+//
+//         $.ajax({
+//           url:'cart',                                       //route
+//           data:{},
+//           method:"GET"
+//         })
+//         .done(function(cResult) {
+//
+//              $('#UserCart').html(cResult);
+//
+//         })
+//         .fail(function(jqXHR, textStatus){
+//
+//             alert('Error:'+ jqXHR +' '+ textStatus);
+//
+//         });
+//   }
+
 //Edit user popup
-function EditLine(UserID,Fristname,Lastname,UserDOB,Gender){
+function EditLine(UserID,Fristname,Lastname,Address,Username,Password,Email,Tel,Privilege){
      $('#myModalLabel').text('EDIT USER');
      $('#sFirstname').val(Fristname);
      $('#sLastname').val(Lastname);
-     $('#sDob').val(UserDOB);
-     $('#gender').val(Gender);
+     $('#sAddress').val(Address);
+     $('#sUsername').val(Username);
+     $('#sPassword').val(Password);
+     $('#sEmail').val(Email);
+     $('#sTel').val(Tel);
      $('#UserID').val(UserID);
-     $("input:radio").prop("checked", false);
-     $("input:radio[value="+Gender+"]").prop("checked", true);
      $('#action_mode').val(2);
+     $('#sPrivilege option[value='+Privilege+']').attr('selected','selected');
 
 }
 
-function select_gender(gender){
-         $("#gender").val(gender);
+function SignIn(){
+    var username = $('#username').val();
+    var password = $('#password').val();
+    console.log('validated '+ username + password);
+         $.ajax({
+           url:'login',
+           data:{username:username,password:password},
+           method:"POST"
+         })
+         .done(function(data){
+           console.log(data);
+           if (data.trim() == 'false'){
+
+              alert('!username or pw incorect');
+           }else {
+             switch (data.trim()) {
+                case '':
+                         window.location='home';
+                break;
+
+                case '1':
+                         window.location='home';
+                break;
+
+                case '2':
+                          window.location='user';
+                break;
+
+                case '3':
+                         window.location='user';
+                break;
+             }
+           }
+          //  $('#UserList').html(data);
+           $('#sModal').modal('hide');
+           $('body').removeClass('modal-open');
+           $('.modal-backdrop').remove();
+         })
+         .fail(function(jqXHR, textStatus){
+             alert('Error:'+ jqXHR +' '+ textStatus);
+         });
+
 }
 
 // show userlist
 function userlist(){
   var fristname_f = $('#fristname_f').val();
   var lastname_f = $('#lastname_f').val();
-  var gender_f = $('#gender_f').val();
+  var email_f = $('#email_f').val();
+
       $.ajax({
         url:'user_list',                                       //route
-        data:{fristname_f:fristname_f,lastname_f:lastname_f,gender_f:gender_f},
+        data:{fristname_f:fristname_f,lastname_f:lastname_f,email_f:email_f},
         method:"GET"
       })
       .done(function(tResult) {
@@ -57,22 +154,46 @@ function register(){
           $('#sFirstname').focus();
       }
 
-      else if ($('#sLastname').val().length == 0) {
-          $('#sLastname').focus();
+     else if ($('#sLastname').val().length == 0) {
+              $('#sLastname').focus();
+            }
+
+     else if ($('#sAddress').val().length == 0) {
+              $('#sAddress').focus();
+            }
+
+     else if ($('#sUsername').val().length == 0) {
+              $('#sUsername').focus();
+            }
+
+     else if ($('#sPassword').val().length == 0){
+              $('#sPassword').focus();
+            }
+
+     else if ($('#sEmail').val().length == 0) {
+              $('#sEmail').focus();
+            }
+
+     else if ($('#sTel').val().length == 0){
+              $('#sTel').focus();
 
       }else{
             console.log('validate done');                      //validate process
             var UserID = $('#UserID').val();
             var sFirstname = $('#sFirstname').val();
             var sLastname = $('#sLastname').val();
-            var sDob = $('#sDob').val();
-            var sGender = $('#gender').val();
+            var sAddress = $('#sAddress').val();
+            var sUsername = $('#sUsername').val();
+            var sPassword = $('#sPassword').val();
+            var sEmail = $('#sEmail').val();
+            var sTel = $('#sTel').val();
+            var sPrivilege = $('#sPrivilege').val();
             var action_mode = $('#action_mode').val();
             console.log(action_mode);                          //check mode 1 add or 2 edit
-            console.log(sDob+'+'+sGender);                     //check dob and gender parameter
+            console.log('firstname: ' + sFirstname + ' lastname: ' + sLastname + ' username: ' + sUsername + ' password: ' +  sPassword + ' Email: '+ sEmail + ' Tel: ' + sTel + sPrivilege);                     //check dob and gender parameter
             $.ajax({
               url:'save',
-              data:{UserID:UserID,sFirstname:sFirstname,sLastname:sLastname,sDob:sDob,sGender:sGender,action_mode:action_mode},
+              data:{UserID:UserID,sPrivilege:sPrivilege,sFirstname:sFirstname,sLastname:sLastname,sAddress:sAddress,sUsername:sUsername,sPassword:sPassword,sEmail:sEmail,sTel:sTel,action_mode:action_mode},
               method:"post"
             })
             .done(function(tResult) {
@@ -83,13 +204,18 @@ function register(){
                     // clear form
                     $('#sFirstname').val('');
                     $('#sLastname').val('');
-                    $('#sDob').val('');
-                    $('#sDob').val('male');
+                    $('#sAddress').val('');
+                    $('#sUsername').val('');
+                    $('#sPassword').val('');
+                    $('#sEmail').val('');
+                    $('#sTel').val('');
+                    $('#sPrivilege').val();
                     $('#action_mode').val(1);
                     // close popup form
-                    $('#userform').modal('hide');
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
+                     $('#userform').modal('hide');
+                     $('body').removeClass('modal-open');
+                     $('.modal-backdrop').remove();
+                  //  $('#obtcls').click();
                     //update user grid
                     userlist();
                   }
@@ -121,127 +247,77 @@ function register(){
       }
 
 }
-=======
-$(document).ready(function(){
-  //console.log('document load done.');
-  userlist();
 
-  $('#btn-register').click(function(){
-      register();
-  });
-});
-//show ADD new user popup modal
-function Addnewuser(){
-     $('#myModalLabel').text('ADD NEW USER');
-}
-//Edit user popup
-function EditLine(UserID,Fristname,Lastname,UserDOB,Gender){
-     $('#myModalLabel').text('EDIT USER');
-     $('#sFirstname').val(Fristname);
-     $('#sLastname').val(Lastname);
-     $('#sDob').val(UserDOB);
-     $('#gender').val(Gender);
-     $('#UserID').val(UserID);
-     $("input:radio").prop("checked", false);
-     $("input:radio[value="+Gender+"]").prop("checked", true);
-     $('#action_mode').val(2);
-
-}
-
-function select_gender(gender){
-         $("#gender").val(gender);
-}
-
-// show userlist
-function userlist(){
-  var fristname_f = $('#fristname_f').val();
-  var lastname_f = $('#lastname_f').val();
-  var gender_f = $('#gender_f').val();
-      $.ajax({
-        url:'user_list',                                       //route
-        data:{fristname_f:fristname_f,lastname_f:lastname_f,gender_f:gender_f},
-        method:"GET"
-      })
-      .done(function(tResult) {
-
-           $('#UserList').html(tResult);
-
-      })
-      .fail(function(jqXHR, textStatus){
-
-          alert('Error:'+ jqXHR +' '+ textStatus);
-
-      });
-}
-//register
-function register(){
+function register2(){
 
       if ($('#sFirstname').val().length == 0) {
           $('#sFirstname').focus();
       }
 
-      else if ($('#sLastname').val().length == 0) {
-          $('#sLastname').focus();
+     else if ($('#sLastname').val().length == 0) {
+              $('#sLastname').focus();
+            }
 
-      }else{
-            console.log('validate done');                      //validate process
-            var UserID = $('#UserID').val();
-            var sFirstname = $('#sFirstname').val();
-            var sLastname = $('#sLastname').val();
-            var sDob = $('#sDob').val();
-            var sGender = $('#gender').val();
-            var action_mode = $('#action_mode').val();
-            console.log(action_mode);                          //check mode 1 add or 2 edit
-            console.log(sDob+'+'+sGender);                     //check dob and gender parameter
-            $.ajax({
-              url:'save',
-              data:{UserID:UserID,sFirstname:sFirstname,sLastname:sLastname,sDob:sDob,sGender:sGender,action_mode:action_mode},
-              method:"post"
-            })
-            .done(function(tResult) {
-                  console.log('done');                        //check processing
-                  if (tResult == 'error') {
-                      alert('!Cannot to save.');
-                  }else{
-                    // clear form
-                    $('#sFirstname').val('');
-                    $('#sLastname').val('');
-                    $('#sDob').val('');
-                    $('#sDob').val('male');
-                    $('#action_mode').val(1);
-                    // close popup form
-                    $('#userform').modal('hide');
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                    //update user grid
-                    userlist();
-                  }
+     else if ($('#sAddress').val().length == 0) {
+              $('#sAddress').focus();
+            }
 
-            })
-            .fail(function(jqXHR, textStatus){
-                alert('Error:'+ jqXHR +' '+ textStatus);
-            });
+     else if ($('#sUsername').val().length == 0) {
+              $('#sUsername').focus();
+            }
+
+     else if ($('#sPassword').val().length == 0){
+              $('#sPassword').focus();
+            }
+
+     else if ($('#sEmail').val().length == 0) {
+              $('#sEmail').focus();
+            }
+
+     else if ($('#sTel').val().length == 0){
+              $('#sTel').focus();
+
+    }else{
+      console.log('validate done');                      //validate process
+      var UserID = $('#UserID').val();
+      var sFirstname = $('#sFirstname').val();
+      var sLastname = $('#sLastname').val();
+      var sAddress = $('#sAddress').val();
+      var sUsername = $('#sUsername').val();
+      var sPassword = $('#sPassword').val();
+      var sEmail = $('#sEmail').val();
+      var sTel = $('#sTel').val();
+      var sPrivilege = $('#sPrivilege').val();
+      var action_mode = $('#action_mode').val();
+      console.log(action_mode);                          //check mode 1 add or 2 edit
+      console.log('firstname: ' + sFirstname + ' lastname: ' + sLastname + ' username: ' + sUsername + ' password: ' +  sPassword + ' Email: '+ sEmail + ' Tel: ' + sTel );                     //check dob and gender parameter
+      $.ajax({
+        url:'save',
+        data:{UserID:UserID,sPrivilege:sPrivilege,sFirstname:sFirstname,sLastname:sLastname,sAddress:sAddress,sUsername:sUsername,sPassword:sPassword,sEmail:sEmail,sTel:sTel,action_mode:action_mode},
+        method:"post"
+      })
+      .done(function(tResult) {
+        console.log('done');                        //check processing
+        if (tResult == 'error') {
+        }else{
+            // clear form
+          $('#sFirstname').val('');
+          $('#sLastname').val('');
+          $('#sAddress').val('');
+          $('#sPassword').val('');
+          $('#sEmail').val('');
+          $('#sTel').val('');
+          $('#action_mode').val(1);
+          // close popup form
+          $('#userform').modal('hide');
+          $('body').removeClass('modal-open');
+          $('.modal-backdrop').remove();
+          alert('Thank you ' + sFirstname + ', your account successfully created !');
+          $('body').load('home');
+          }
+        })
+        .fail(function(jqXHR, textStatus){
+            alert('Error:'+ jqXHR +' '+ textStatus);
+        });
       }
 }
-//Delete User
- function DeleteUser(UserID,Fristname){
-      if (confirm('Are you sure to delete user ' + Fristname)) {
-            $.ajax({
-              url:'remove',
-              data:{UserID:UserID,Fristname:Fristname},
-              method:"GET"
-            })
-            .done(function(tResult) {
-                 if (tResult == 'error') {
-                     alert('!Cannot delete user.');
-                 }else{
-                     userlist();
-                 }
-            })
-            .fail(function(jqXHR, textStatus){
-                alert('Error:'+ jqXHR +' '+ textStatus);
-            });
-      }
-
-}
->>>>>>> test commit
