c49db88339688774baa43baa8bcc3275f87f75bf
Brightbite <zindybiteme@gmail.com>
2017-09-16 00:57:11 +1000

    Checkout and Shipping

--- controllers/cCheckout.php
+++ controllers/cCheckout.php
@@ -7,18 +7,13 @@ class cCheckout extends CI_Controller {
           parent::__construct();
           $this->load->library('cart');
           $this->load->library('session');
+          // $this->load->library('paypalexpress');
+          $this->load->model('mOrder','MOrder');
           $this->load->helper('url');
-          $this->load->model('mCart','MCart'); //load model first before view
   }
 
   public function index()
   {
-    if ($this->session->has_userdata('PrivilegeID')) {
-        $PrivilegeID = $this->session->userdata('PrivilegeID');
-    }else {
-        $PrivilegeID = '';
-    }
-
     if ($this->session->has_userdata('customerNameSess')) {
         $custname = $this->session->userdata('customerNameSess');
     }else {
@@ -31,59 +26,130 @@ class cCheckout extends CI_Controller {
         $custlast = '';
     }
 
-    if ($this->session->has_userdata('csOderID')) {
-        $custOdId= $this->session->userdata('csOderID');
+    if ($this->session->has_userdata('PrivilegeID')) {
+        $PrivilegeID = $this->session->userdata('PrivilegeID');
     }else {
-        $custOdId = '';
-    }
-    if ($this->session->has_userdata('csOderNumber')) {
-        $custOdNum = $this->session->userdata('csOderNumber');
-    }else {
-        $custOdNum = '';
-    }
-    if ($this->session->has_userdata('csID')) {
-        $custID = $this->session->userdata('csID');
-    }else {
-        $custID = '';
+        $PrivilegeID = '';
     }
 
 
     $header = array(
-      'title' => 'My Account',
-      'keywords' => 'account',
+      'title' => 'Checkout',
+      'keywords' => 'checkout',
       'description' => 'this is web application for online retailer',
       'author' => 'Kunanon Pititheerachot #12634123 UTS',
       'custname'=> $custname,
-      'custlast'  => $custlast
+      'custlast'  => $custlast,
+      'privid' => $PrivilegeID
 
     );
 
+    $shippingFirstname = $this->input->post('sFname');
+    $shippingLastname = $this->input->post('sLname');
+    $shippingAddr = $this->input->post('sAddr');
+    $shippingAddr2 = $this->input->post('sAddr2');
+    $shippingCity = $this->input->post('sCity');
+    $shippingState = $this->input->post('sState');
+    $shippingPostcode = $this->input->post('sPostcode');
+    $shippingCountry = $this->input->post('sCountry');
+    $shippingEmail = $this->input->post('sEmails');
+    $shippingTel = $this->input->post('sTels');
 
-      // $productdata = array(
-    //   //             'id'      => 'sku_123ABC',
-    //   //             'qty'     => 1,
-    //   //             'price'   => 39.95,
-    //   //             'name'    => 'T-Shirt',
-    //   //             'options' => array('Size' => 'L', 'Color' => 'Red')
-    //   // );
-    //   //
-    //   // $this->cart->insert($productdata);
-    //
-    // $data = array();
+    $address = array('shippingFirstname' => $shippingFirstname,
+                      'shippingLastname' => $shippingLastname,
+                      'shippingAddr' => $shippingAddr,
+                      'shippingAddr2' => $shippingAddr2,
+                      'shippingCity' => $shippingCity,
+                      'shippingState' => $shippingState,
+                      'shippingPostcode' => $shippingPostcode,
+                      'shippingCountry' => $shippingCountry,
+                      'shippingEmail' => $shippingEmail,
+                      'shippingTel' => $shippingTel
+                      );
+
+    $this->session->set_userdata($address);
     $this->load->view('template/header',$header);
-    // // $this->load->view('vCart',$index); //load view
-    // $cartData = array();
-    // $cartData['cartUser'] = $this->MCart->mCartuser();
-    $this->load->view('vCheckout');
-    $this->load->view('template/footer');
-    // // $pvlList = $this->MCart->mCartuser();
+
+    $this->load->view('vCheckout',$address);
+    $this->load->view('template/cartfooter');
+
   }
 
-  // public function usercart(){
-  //           $cartData = array();
-  //           $cartData['cartUser'] = $this->MCart->mCartuser();
-  //           $this->load->view('vCart',$cartData);
+  // public function saveorder($paymentType){
   //
+  //
+  //   echo $this->session->userdata('shippingFirstname');
+  //
+  //
+  // }
+
+  public function saveorder($paymentType){
+
+        if ($this->session->has_userdata('customerIDSess')) {
+            $custID = $this->session->userdata('customerIDSess');
+        }else {
+            $custID = '';
+        }
+
+        if ($this->session->has_userdata('shippingFirstname')) {
+            $shipFirstName = $this->session->userdata('shippingFirstname');
+        }else {
+            $shipFirstName = '';
+        }
+
+          $olastOrder =  $this->MOrder->mgetOrderID();
+          $tlastOrder = $olastOrder->OrderID;
+          date_default_timezone_set("Australia/Sydney");
+          //echo $tlastOrder;
+          $cDate = date('d');
+          $cMonth = date('m');
+          $cYear = date('Y');
+          $cTime = date('H:i:s');
+          if ($tlastOrder == 'empty') {
+              $nOrder = $cDate.$cMonth.$cYear.'-1';
+          }else{
+
+              $aLastOrder = explode("-",$tlastOrder);
+              echo $aLastOrder[1];
+              $nNext = $aLastOrder[1] + 1;
+              $nOrder = $aLastOrder[0].'-'.$nNext;
+          }
+
+          $OrderDate = $cYear.'-'.$cMonth.'-'.$cDate.' '.$cTime;
+
+          //save order
+          $OrderData = array('OrderID' => $nOrder,
+                             'CustomerID' => $custID,
+                             'OrderTotal' => $this->cart->total(),
+                             'PaymentType' => $paymentType,
+                             'ShippingName' => $shipFirstName,
+                             'OrderDate' => $OrderDate
+         );
+         $this->MOrder->msaveOrder($OrderData);
+
+          if ($cart = $this->cart->contents()){
+              foreach ($cart as $item){
+                       $order_detail = array(
+                       'OrderID' => $nOrder,
+                       'ProductID' => $item['id'],
+                       'Quantity' => $item['qty'],
+                       'Price' => $item['price']
+                       );
+
+                       echo var_dump($order_detail);
+
+                       // Insert product imformation with order detail, store in cart also store in database.
+
+                      $this->MOrder->msaveOrderDetail($order_detail);
+             }
+
+             $this->cart->destroy();
+          }
+
+          redirect('home');
+
 
   }
+
+}
 ?>
--- views/vCheckout.php
+++ views/vCheckout.php
@@ -1,31 +1,126 @@
-<!-- Page Content -->
 <div class="container">
-
   <div class="row">
-
-    <div class="col-lg-3">
-      <h1 class="my-4">Order Summary</h1>
+    <div class="col-md-12">
+      <!-- col-md-12 -->
+      <h3 class="text-info"><i class="fa fa-shopping-basket fa-3x" aria-hidden="true"></i> Checkout</h3><hr>
     </div>
-    <!-- /.col-lg-3 -->
 
-    <div class="col-lg-9">
-
-      <div class="card mt-4">
-        <!-- <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt=""> -->
-        <div class="card-body">
-          <h3 class="card-title">Order detail</h3>
-          <button type="button" name="button" class="btn btn-success pull-right">Confirm Payment</button><br>
-        </div>
+    <div class="col-md-6 text-secondary">
+      <h5 class="text-secondary"><i class="fa fa-truck" aria-hidden="true"></i> Delivery to</h5><hr>
+        Firstname:       <?php echo $shippingFirstname;?><br>
+        Lastname:        <?php echo $shippingLastname;?><br>
+        Address Line 1:  <?php echo $shippingAddr;?><br>
+        Address Line 2:  <?php echo $shippingAddr2;?><br>
+        City:            <?php echo $shippingCity;?><br>
+        State:           <?php echo $shippingState;?><br>
+        Postcode:        <?php echo $shippingPostcode;?><br>
+        Country:         <?php echo $shippingCountry;?><br>
+        Email:           <?php echo $shippingEmail;?><br>
+        Contact Number:  <?php echo $shippingTel;?><br>
+        <!-- <input type="text" id="payment_method" value="1"> -->
       </div>
-      <!-- /.card -->
+    <div class="col-lg-6 text-secondary">
+      <h5 class="text-dark"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Payment Method</h5><hr>
+      <div class="custom-controls-stacked">
+        <small class="text-muted">
+          Choose your payment method.
+        </small><br>
+        <label class="custom-control custom-radio">
+          <input name="payMethod" type="radio" class="custom-control-input" value="" checked onclick="select_credit()">
+          <span class="custom-control-indicator"></span>
+          <span class="custom-control-description">
+            <i class="text-info fa fa-cc-visa fa-2x" aria-hidden="true"></i>
+            <i class="text-success fa fa-cc-mastercard fa-2x" aria-hidden="true"></i>
+            <i class="text-primary fa fa-cc-amex fa-2x" aria-hidden="true"></i>
+            <i class="text-warning fa fa-cc-discover fa-2x" aria-hidden="true"></i><br>
+            <small class="text-muted">
+              Credit or debit card.
+            </small>
+          </span>
+        </label><br>
+        <label class="custom-control custom-radio">
+          <input name="payMethod" type="radio" class="custom-control-input" value="" onclick="select_paypal()">
+          <span class="custom-control-indicator"></span>
+          <span class="custom-control-description">
+            <i class="text-primary fa fa-cc-paypal fa-2x" aria-hidden="true"></i><br>
+            <small class="text-muted">
+              Paypal.
+            </small>
+          </span>
 
-
-      <!-- /.card -->
-
+        </label>
+        <input type="hidden" name="" id="payMethod" value="credit">
+      </div>
+    </div>
+    <div class="col-lg-12 text-secondary"><hr>
+      <h5 class="text-secondary"><i class="fa fa-opencart" aria-hidden="true"></i> Item Summary</h5><hr>
+      <table cellpadding="6" cellspacing="1" style="width:100%" border="0">
+      <tr>
+           <th class="text-secondary">Product ID</th>
+           <th class="text-secondary">Picture</th>
+           <th class="text-secondary">Item Description</th>
+           <th class="text-secondary">QTY</th>
+           <th class="text-secondary" style="text-align:right">Item Price</th>
+           <th class="text-secondary" style="text-align:right">Sub-Total</th>
+      </tr>
+      <?php $i = 1; ?>
+      <?php foreach ($this->cart->contents() as $items): ?>
+           <?php echo form_hidden('rowid'.$i, $items['rowid']); ?>
+           <tr>
+              <td class="text-info"># <?php echo $items['id'];?></td>
+              <td></?php echo $items['image'];?></td>
+                   <td class="text-dark"><h6>
+                           <?php echo $items['name']; ?>
+                           <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+                                   <p>
+                                           <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+                                                   <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+                                           <?php endforeach; ?>
+                                   </p>
+                           <?php endif; ?>
+                   </td></h6>
+                   <td><?php echo form_input(array('name' =>'qty'.$i, 'value' => $items['qty'], 'maxlength' => '3', 'size' => '3')); ?></td>
+                   <td style="text-align:right"><?php echo $this->cart->format_number($items['price']); ?></td>
+                   <td style="text-align:right"><i class="fa fa-usd text-success"></i><?php echo $this->cart->format_number($items['subtotal']); ?></td>
+           </tr>
+      <?php $i++; ?>
+      <?php endforeach; ?>
+      <tr>
+           <td colspan="3"> </td>
+           <td class="pull-right text-dark"><strong>Total</strong></td>
+           <td class="" style="text-align:right"><i class="fa fa-usd text-success"></i> <?php echo $this->cart->format_number($this->cart->total()); ?></td>
+      </tr>
+      </table>
+    </div>
+    <div class="col-lg-12 text-secondary"><hr>
+      <button type="button" class="btn btn-light pull-left" onclick="backtoship()"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back to Shipping Address</button>
+      <button type="button" class="btn btn-primary pull-right" onclick="payment()">Confirm and Pay <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
     </div>
     <!-- /.col-lg-9 -->
-
   </div>
-
 </div>
 <!-- /.container -->
+
+<script type="text/javascript">
+        function backtoship(){
+            window.location = 'shipping';
+        }
+
+        function select_paypal(){
+            $('#payment_method').val(2);
+        }
+
+        function select_credit(){
+            $('#payment_method').val(1);
+        }
+
+        function payment(){
+            if ($('#payment_method').val()==1){
+                window.location = 'credit';
+            }else{
+               window.location = 'cpaypal';
+            }
+
+        }
+
+</script>
