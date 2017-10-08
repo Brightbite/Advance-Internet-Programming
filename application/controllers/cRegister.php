9e46c42e22e53d62ca87ec2f91c992efb37ee8cd
Brightbite <zindybiteme@gmail.com>
2017-10-09 03:40:41 +1100

    fix error from model register page

--- application/controllers/cRegister.php
+++ application/controllers/cRegister.php
@@ -6,7 +6,6 @@ class cRegister extends CI_Controller {
   function __construct(){
           parent::__construct();
           $this->load->helper('url');
-          $this->load->model('mRegister','MRegister');
   }
 
   public function index()
